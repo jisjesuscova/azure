03bed2385787bb04f5cd4c3ce31dbb4746d21f49
jisjesuscova <jesuscova@jisparking.com>
2022-11-27 19:21:56 -0300

    cambios

--- conexion.php
+++ conexion.php
@@ -1,6 +1,6 @@
 <?php
 $conn = mysqli_init();
-mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/ssl/BaltimoreCyberTrustRoot.crt.pem", NULL, NULL);
+mysqli_ssl_set($conn,NULL,NULL, "ssl/BaltimoreCyberTrustRoot.crt.pem", NULL, NULL);
 mysqli_real_connect($conn, 'laravel-jesus-cova-huerta.mysql.database.azure.com', 'jesuscovaroot@laravel-jesus-cova-huerta', 'admin2022!', 'base_datos', 3306, MYSQLI_CLIENT_SSL);
 if (mysqli_connect_errno($conn)) {
 die('Failed to connect to MySQL: '.mysqli_connect_error());
