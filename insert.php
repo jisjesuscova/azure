6fb0af4b800ebb97d4e8dc01347cbb27a75d5aa2
jisjesuscova <jesuscova@jisparking.com>
2022-11-27 18:18:15 -0300

    Nuevo

---
+++ insert.php
@@ -0,0 +1,12 @@
+<?php
+include('conexion.php');
+
+$id = 1;
+$numero = 200;
+$duenos = 'Pedro Luis';
+
+$query = "INSERT INTO test (id, numero, duenos) VALUES (".$id.", ".$numero.", '".$duenos."')";
+
+mysqli_query($conn, $query) or die(mysql_error());
+
+header("Location: index.php");
