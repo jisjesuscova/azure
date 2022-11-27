fcbd5279cfa48029e9b7cc458d2f296b58e6750a
jisjesuscova <jesuscova@jisparking.com>
2022-11-27 18:52:07 -0300

    cambios

--- guardar.php
+++ guardar.php
@@ -1,2 +1,12 @@
 <?php
-echo 1;
+include('conexion.php');
+
+$id = 1;
+$numero = 200;
+$duenos = 'Pedro Luis';
+
+$query = "INSERT INTO test (id, numero, duenos) VALUES (".$id.", ".$numero.", '".$duenos."')";
+
+mysqli_query($conn, $query) or die(mysql_error());
+
+header("Location: index.php");
